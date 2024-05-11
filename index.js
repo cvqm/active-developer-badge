1c86b5cfb8617d2e1693603a118a9b5ca7cf5cd3
cvqm <122008140+cvqm@users.noreply.github.com>
2024-05-11 15:39:18 +1200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const { GatewayIntentBits, Client, Events, REST, Routes, SlashCommandBuilder } = require("discord.js");
+const { token, id } = require("./src/config.json");
+const fs = require("fs");
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+const rest = new REST({ version: "10"}).setToken(token);
+
+client.on(Events.ClientReady, () => {
+    console.log(`The bot has been activated`); //-- console message (optional)
+    client.user.setStatus('dnd'); //-- this is optional for if you want the bot on do not disturb
+});
+
+client.on(Events.InteractionCreate, (interaction) => {
+    if (!interaction.isCommand()) return;
+    if (interaction.user.bot) return;
+
+    interaction.reply('pong');
+});
+
+(async () => {
+    try {
+        await rest.put(Routes.applicationCommands(id), {body: [
+            new SlashCommandBuilder().setName("ping").setDescription("responds with 'pong'"),
+        ]});
+    } catch (err) {
+        console.warn(err)    
+    }
+})()
+
+client.login(token);
